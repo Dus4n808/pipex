34ea4641f9922c0d0947e22ec0869080a863eb01
Dusan <dusanfama@gmail.com>
2025-11-17 19:03:45 +0100

    fini

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: dufama <dufama@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 16:08:57 by dufama            #+#    #+#             */
-/*   Updated: 2025/11/17 18:33:52 by dufama           ###   ########.fr       */
+/*   Updated: 2025/11/17 18:38:22 by dufama           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ char	*find_cmd(char *cmd, char **envp);
 void	execute_cmd(char *cmd, char **envp);
 int		open_infile(char *file);
 int		open_outfile(char *file);
-void	child_procces(int input_fd, int output_fd, char *cmd, char **envp);
+void	child_process(int input_fd, int output_fd, char *cmd, char **envp);
 void	run_child(t_data *pipex, int pipe_fd[2], char **envp, int i);
 void	init_struct(t_data *pipex, int argc, char **argv);
 void	clean_parent(t_data *pipex, int pipe_fd[2], int i);
